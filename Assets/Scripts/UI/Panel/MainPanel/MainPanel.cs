1c394ed8e3cc89c8d8c2ae5d21d6ac3ab3d8be71
xiazm <xiazm@citic.com>
2026-01-17 11:22:44 +0800

    显示MainPanel

--- Assets/Scripts/UI/Panel/MainPanel/MainPanel.cs
+++ Assets/Scripts/UI/Panel/MainPanel/MainPanel.cs
@@ -2,22 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using WildBoar.GUIModule;
 
 public class MainPanel : PanelBase
 {
     public SceneUnitHUD EnemyHUD;
     public void Start()
     {
-        //TODO 是不是可以直接放在一个场景里，而不用调用这个函数
-        //因为调用后，这些对象好像是被放在了另外一个场景里。
+        
         DontDestroyOnLoad(this.gameObject);
         EnemyHUD.Hide();
 
+        ConfigureUICameraOverlay();
     }
 
     public void OnEnable()
     {
         MouseManager.OnClickSceneUnitMaybeNull += OnClickSceneObj;
+        ConfigureUICameraOverlay();
     }
 
     public void OnDisable()
@@ -27,8 +30,6 @@ public class MainPanel : PanelBase
 
     public void OnClickSceneObj(IHasSceneUnitInfo sceneUnitInfoOwner)
     {
-        //检查是不是需要显示HUD的GameObj
-        //TODO 将该物体放置到其他sprite前面，突出显示
         if (sceneUnitInfoOwner == null)
         {
             EnemyHUD.Hide();
@@ -37,6 +38,38 @@ public class MainPanel : PanelBase
 
         EnemyHUD.Show();
         EnemyHUD.SetInfo(sceneUnitInfoOwner.GetSceneUnit());
+    }
+
+    private void ConfigureUICameraOverlay()
+    {
+        var canvas = GetComponent<NUICanvas>();
+        if (canvas == null) return;
+        var uiCam = canvas.Camera;
+        if (uiCam == null) return;
+
+        var uiData = uiCam.GetComponent<UniversalAdditionalCameraData>();
+        if (uiData != null) uiData.renderType = CameraRenderType.Overlay;
+        uiCam.clearFlags = CameraClearFlags.Depth;
+
+        var baseCam = Camera.main;
+        if (baseCam == null)
+        {
+            var cams = GameObject.FindObjectsOfType<Camera>();
+            foreach (var c in cams)
+            {
+                if (c != uiCam && c.enabled && c.targetTexture == null)
+                {
+                    baseCam = c;
+                    break;
+                }
+            }
+        }
+        if (baseCam == null) return;
+
+        var baseData = baseCam.GetComponent<UniversalAdditionalCameraData>();
+        if (baseData == null) return;
+        if (!baseData.cameraStack.Contains(uiCam)) baseData.cameraStack.Add(uiCam);
+        baseData.renderType = CameraRenderType.Base;
     }
 
 
